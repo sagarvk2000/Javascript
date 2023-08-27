ed563b991d28cd3448c03d3e03c045004b3c3904
sagarvk2000 <136054368+sagarvk2000@users.noreply.github.com>
2023-08-27 12:10:00 +0530

    Add files via upload

---
+++ sagar javacript/index.js
@@ -0,0 +1,48 @@
+////// JavaScript source code
+////var a = 20;
+////let b = 10;
+////const pi = 3.14;
+
+////var name = "sagar";
+////var isAdmin = false;
+////var salary = 45555.88;
+////var id = 2011;
+
+////// two ways to print
+////document.write("my name is " + name);
+
+////console.log("my name is " + name);
+////for (var i = 1; i <= 10; i++) {
+////    document.write("<br/>" + i);
+////}
+
+//////functions in javascript
+////function Addition() {
+////    var a = 10, b = 20;
+////    var c = a + b;
+////    document.write("<br/>"+c);
+////}
+//////to call the function
+////Addition();
+
+////function Sub(a, b) {
+////    var c = a - b;
+////    document.write("<br/>" + c);
+////}
+////Sub(30, 10);
+
+////function Multiply(a, b) {
+////    var c = a * b;
+////    return c;
+////}
+////var result = Multiply(2, 3);
+////document.write("<br/>" + result);
+
+////function div(a, b) {
+////    var c = a / b;
+////    return c;
+////}
+////var result = div(10, 5);
+////document.write("<br/>" + result);
+
+
